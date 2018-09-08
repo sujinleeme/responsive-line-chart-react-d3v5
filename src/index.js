8aac5a73c6edb9223c47395e945b7e600763bcd6
sujinleeme <sujinlee.me@gmail.com>
2018-09-08 15:59:36 +0900

    add hot module replacement

--- src/index.js
+++ src/index.js
@@ -5,5 +5,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
 registerServiceWorker();
